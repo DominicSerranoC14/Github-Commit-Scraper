1a7781fc724d8115eceace29cf1a9820c26fd8a2
Dominic Serrano <dmsrojo@gmail.com>
2016-09-09 11:56:12 -0500

    Server is now rendering layout.pug

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const router = Router();
 // Route for '/'
 router.get('/', (req, res) => {
   //Will render the index file in the views dir
-  res.render(`Testing server`);
+  res.render('layout.pug');
 });
 /////////////////////////////////////////
 
